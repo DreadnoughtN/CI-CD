eaa38d4abdd9ea103c247f7e11a9ce3e26560dd6
Nikitin DM <Nikitindm21@st.ithub.ru>
2024-01-26 14:02:53 -0500

    code updated

--- CP1/queens.cpp
+++ CP1/queens.cpp
@@ -3,48 +3,33 @@ using namespace std;
 
 bool queen2(int x1, int y1, int x2, int y2)
 {
-    bool flag = false;
-
-    if ((x1 == x2 or y1 == y2) or (abs(x1 - x2) == abs(y1 - y2)))
-    {
-        flag = true;
-    }
-
-    return flag;
+    if ((x1 == x2 or y1 == y2) or (abs(x1 - x2) == abs(y1 - y2))) {return true;}
+    return false;
 }
 
 bool queen3(int x1, int y1, int x2, int y2, int x3, int y3)
 {
-    bool flag = false;
-
-    if (queen2(x1, y1, x2, y2) and queen2(x1, y1, x3, y3) and queen2(x2, y2, x3, y3))
-    {
-        flag = true;
-    }
-
-    return flag;
+    if (queen2(x1, y1, x2, y2) and queen2(x1, y1, x3, y3) and queen2(x2, y2, x3, y3)) {return true;}
+    return false;
 }
 
 bool valid_input(int x)
 {
+    if (x > 8 or x < 1) {return false;}
+    return true;
+}
 
-    if (x > 8 or x < 1)
-    {
-        return false;
-    }
-
+bool valid_place(int x1, int y1, int x2, int y2)
+{
+    if (x1 == x2 and y1 == y2) {return false;}
     return true;
 }
 
 int main()
 {
-
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-    int x3;
-    int y3;
+    int x1, y1;
+    int x2, y2;
+    int x3, y3;
     cout << "Enter coordinates 1.Queen (1 >= x,y <= 8): ";
     cin >> x1 >> y1;
     if (!valid_input(x1) or !valid_input(y1))
@@ -54,14 +39,14 @@ int main()
     }
     cout << "Enter coordinates 2.Queen (1 >= x,y <= 8): ";
     cin >> x2 >> y2;
-    if (!valid_input(x2) or !valid_input(y2))
+    if ((!valid_input(x2) and !valid_input(y2)) and !valid_place(x1, y1, x2, y2))
     {
         cout << "Incorrect input";
         return 0;
     }
     cout << "Enter coordinates 3.Queen (1 >= x,y <= 8): ";
     cin >> x3 >> y3;
-    if (!valid_input(x3) or !valid_input(y3))
+    if (!valid_input(x3) and !valid_input(y3) and !valid_place(x1, y1, x3, y3) and !valid_place(x2, y2, x3, y3))
     {
         cout << "Incorrect input";
         return 0;
